98d3264bc3a88bd5f9fc421d78b446425ae94fe6
Joshua Cox <joshua.n.cox@gmail.com>
2016-01-14 01:33:34 -0800

    update:docs updated package description

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ function getXML(id) {
 };
 
 //db
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log('Listening on port ' + PORT + ' ...');  
   }); 
