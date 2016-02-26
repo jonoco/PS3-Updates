856ac86806f923204273abb0c08abfbf613caf94
Joshua Cox <joshua.n.cox@gmail.com>
2016-02-25 16:17:01 -0800

    fix:db need to wipe production db for game model change

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ function getXML(id) {
 };
 
 //db
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log('Listening on port ' + PORT + ' ...');  
   }); 
